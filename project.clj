c5810bb6298000001e0ac862dd8c18a413b9de80
Martin Klepsch <martinklepsch@googlemail.com>
2014-12-17 21:40:04 +0100

    project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tenzing/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojurescript application template built on Boot"
+  :url "http://github.com/martinklepsch/tenzing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
