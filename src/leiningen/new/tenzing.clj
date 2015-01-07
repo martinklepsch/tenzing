e59ae1c35bbccbce3a562a048a0ec9bd931e047a
Martin Klepsch <martinklepsch@googlemail.com>
2015-01-07 22:03:13 +0100

    update om

--- src/leiningen/new/tenzing.clj
+++ src/leiningen/new/tenzing.clj
@@ -78,8 +78,8 @@
 
 (defn dependencies [opts]
   (cond-> []
-          (om?      opts) (conj "om \"0.7.3\"" "cljsjs/react \"0.11.2\"")
-          (reagent? opts) (conj "reagent \"0.4.3\"" "cljsjs/react \"0.12.2-1\"")
+          (om?      opts) (conj "om \"0.8.0-rc1\"" "cljsjs/react \"0.12.2-3\"")
+          (reagent? opts) (conj "reagent \"0.4.3\"" "cljsjs/react \"0.12.2-3\"")
           (garden?  opts) (conj "boot-garden \"1.2.5-1\"")
           (sass?    opts) (conj "boot-sassc  \"0.1.0\"")
           (cljsjs?  opts) (conj "cljsjs/boot-cljsjs \"0.4.0\"")))
