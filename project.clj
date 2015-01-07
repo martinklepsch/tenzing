f865c4703dd1c90379c3608c445cd5669101b7f9
Martin Klepsch <martinklepsch@googlemail.com>
2015-01-07 22:26:43 +0100

    cut 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tenzing/lein-template "0.2.0"
+(defproject tenzing/lein-template "0.2.1"
   :description "Clojurescript application template built on Boot"
   :url "http://github.com/martinklepsch/tenzing"
   :license {:name "Eclipse Public License"
