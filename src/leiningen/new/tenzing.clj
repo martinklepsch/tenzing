c24f7d4cf19e214333b0753c56ff6afe104f327c
Martin Klepsch <martinklepsch@googlemail.com>
2015-01-22 16:55:12 +0100

    fix typo

--- src/leiningen/new/tenzing.clj
+++ src/leiningen/new/tenzing.clj
@@ -101,7 +101,7 @@
 
 (defn production-task-opts [opts]
   (cond-> []
-          (cljsjs? opts) (conj (str "cljsjs {:profile :production}"))
+          (cljsjs? opts) (conj (str "from-cljsjs {:profile :production}"))
           (garden? opts) (conj (str "garden {:pretty-print false}"))
           (sass?   opts) (conj (str "sass   {:output-style \"compressed\"}"))))
 
