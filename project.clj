72759b55359a81caf24db231fb61d818437f1fee
Martin Klepsch <martinklepsch@googlemail.com>
2015-04-10 19:58:12 +0200

    remove some stuff not needed for website

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject tenzing/lein-template "0.3.0"
-  :description "Clojurescript application template built on Boot"
-  :url "http://github.com/martinklepsch/tenzing"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
--- resources/leiningen/new/tenzing/app.cljs
+++
@@ -1,6 +0,0 @@
-(ns {{name}}.app)
-
-(defn init []
-  (let [c (.. js/document (createElement "DIV"))]
-    (aset c "innerHTML" "<p>i'm dynamically created</p>")
-    (.. js/document (getElementById "container") (appendChild c))))
