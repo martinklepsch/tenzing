04d7aa6e363c2682f81c87c9b870290aea59840b
Jim Riordan <jim@j1mr10rd4n.info>
2016-05-03 16:23:03 +1000

    bump version to 0.3.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tenzing/lein-template "0.3.6"
+(defproject tenzing/lein-template "0.3.7-SNAPSHOT"
   :description "Clojurescript application template built on Boot"
   :url "http://github.com/martinklepsch/tenzing"
   :license {:name "Eclipse Public License"
